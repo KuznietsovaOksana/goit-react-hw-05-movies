634beb3c65a2b53f7920cecac4d47fa38dec502a
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-01-08 17:34:49 +0200

    Add: modern-normalize, pages template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Movies from '../pages/Movies/Movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 };
