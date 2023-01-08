cc896869b549c4e7ac8aa1700efadea99da2ca94
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-01-08 17:38:34 +0200

    fix: modern-normalize

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
-import './index.css';
 import 'modern-normalize/modern-normalize.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
