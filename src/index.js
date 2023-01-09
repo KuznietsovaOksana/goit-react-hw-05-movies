b2a6d6f8a8d836a8fe241e81ebd3d562ff47c6b4
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-01-09 18:49:37 +0200

    adds: basename

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
-    // basename="/goit-react-hw-05-movies/"
-    >
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
